4a1c4b5353019f410ce31336b22e0992ef2693b8
anamarquez <anamarquez.edu@hotmail.com>
2022-01-12 16:56:30 -0800

    Implementing Automapper

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+        public DateTime DateRequested { get; set; }
+        public bool? Aprroved { get; set; }
+    }
+}
---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
